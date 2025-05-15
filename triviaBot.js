463a64d22abdffbd49f14715066dd944fcea896b
MahdiU258 <177416182+MahdiU258@users.noreply.github.com>
2025-05-15 19:20:51 +0100

    triviaBot.js v1.00

---
+++ triviaBot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "broBot";
+let botLocation = "broHome";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "With " + favoriteLanguage + ", you can add comments to your code with two different methods.";
+
+console.log(codingFact);
+
+codingFact = "Also with " + favoriteLanguage + ", there are three different methods for concatenating strings.";
+
+console.log(codingFact);
+
+codingFact = "Did you know that in " + favoriteLanguage + ", there are eight different data types.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
